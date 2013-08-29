f9e2e1dff7d5a062c0e8cb798b233a3e7f1259a4
sergiosssg <sergiosssg@gmail.com>
2013-08-29 16:45:04 +0300

    Create TaskLounchWrapper.java

---
+++ TaskLounchWrapper.java
@@ -0,0 +1,19 @@
+package TaskManager;
+
+public class TaskLounchWrapper implements Runnable {
+
+	@Override
+	public void run() {
+		
+		try{
+			// here must be usefull code of our task
+		}
+		catch(Exception exception){
+			System.out.println(exception.getStackTrace().toString());
+			
+		}
+
+
+	}
+
+}
