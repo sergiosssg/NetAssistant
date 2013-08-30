8ae229b8449e4afc2afc81a40ccb94cc3a129d01
sergiosssg <sergiosssg@gmail.com>
2013-08-30 14:55:36 +0300

    Update SimpleTaskHere.java

--- SimpleTaskHere.java
+++ SimpleTaskHere.java
@@ -16,21 +16,48 @@ public class SimpleTaskHere implements Runnable {
 	 */
 	
 	String _taskName;
+	RemoteTask _remoteTask;
+	private boolean _emptyName;
+	private boolean _needRemoteExecution;
 	
 	SimpleTaskHere(){
 		System.out.println("Next empty task created.");
+		_emptyName = true;
+		_needRemoteExecution = false;
 	}
 	
 	SimpleTaskHere(String name){
 		_taskName = name;
 		System.out.println("Task" + _taskName + " created.");
+		_emptyName = false;
+		_needRemoteExecution = false;
+	}
+	
+	SimpleTaskHere(String name, RemoteTask remoteTask){
+		_taskName = name;
+		_remoteTask = remoteTask;
+		_emptyName = false;
+		_needRemoteExecution = true;
 	}
 	
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		if (!_emptyName) Thread.currentThread().setName(_taskName);
+		if (_needRemoteExecution){
+			/**
+			 * here need to implement call remote functions,
+			 * as RMI 
+			 */
+			;
+		} else {
+			/**
+			 *  ordinary call local routine
+			 */
+			;
+		}
 
-	}
+
 
 }
