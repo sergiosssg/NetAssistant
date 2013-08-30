256d402fc908e9bd5a8897259722892f8f00945d
sergiosssg <sergiosssg@gmail.com>
2013-08-30 15:10:14 +0300

    Create SimpleTaskRunningRemotly.java

--- TaskLouncher.java
+++ TaskLouncher.java
@@ -5,5 +5,17 @@ public class TaskLouncher {
 	 * Responsible for launching predefined tasks
 	 * 
 	 */
+	static private TaskQueue _taskQueue;
+	
+	public static int executeForever(){
+		/**
+		 * implements infinite loop seeking potetial task need to run
+		 * 
+		 */
+		
+		/*
+		 * as matter of fact 
+		 */
+	}
 
 }
