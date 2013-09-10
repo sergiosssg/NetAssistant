5c17fdf5f0b9a6d47e9c3419f401cb8e56b2d145
sergiosssg <sergiosssg@gmail.com>
2013-09-10 17:02:41 +0300

    Update TaskType.java

--- taskManager/TaskType.java
+++ taskManager/TaskType.java
@@ -8,5 +8,31 @@ package taskManager;
  *
  */
 public interface TaskType {
+  
+
+
+	public static final POLLING;
+	public static final POLLINGHOSTS;
+	public static final POLLINGSERVICES;
+	public static final POLLINGDRIVERS;
+	public static final POLLINGUSERS;
+	public static final POLLINGNETWORKACTIVITY;
+	public static final POLLINGUSERACTIVITY;
+	public static final POLLINGSERVICEACTIVITY;
+	public static final RUNNINGLOCALLY;
+	public static final RUNNINGREMOTELY;
+	public static final REPORTCREATE;
+	public static final REPORTSAVE;
+	public static final REPORTRESTORE;
+	public static final REPORTMODIFY;
+	public static final REPORTGETEROGENIOUS;
+	public static final REPORTGOMOGENIOUS;
+	public static final REPORTSIMPLE;
+	public static final REPORTCOMPLEX;
+	public static final REPORTDISPLAYING;
+	public static final ADMINISTERING;
+	public static final STORAGEADJUSTING;
+	public static final GUIFORMDISPLAYING;
+	public static final WEBFORMDISPLAYING;
 
 }
